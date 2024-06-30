786478b455f2c1b72e697e14574dc45b62d35da9
blacksniper786 <blacksniper786@gmail.com>
2024-06-30 06:34:32 +0100

    pretty good

--- minitalk/client.c
+++ minitalk/client.c
@@ -11,6 +11,8 @@
 /* ************************************************************************** */
 
 #include"minitalk.h"
+#include"stdio.h"
+
 
 void put_message(char *str, int fd)
 {
@@ -27,13 +29,20 @@ int convert_char(char *pid, char *msg)
     counter = 0 ;
     while(msg[counter] != '\0')
     {   
-        count_bit = 0;
-        while( count_bit < 8){
+        count_bit = 7;
+        while( count_bit >= 0){
             if(1 & (msg[counter] >> count_bit))
-                kill(ft_atoi(pid), SIGUSR1);
-            else
+            {   
+                //printf("bit %d \n",( 1 & (msg[counter] >> count_bit)));
                 kill(ft_atoi(pid), SIGUSR2);
-            count_bit++;
+                usleep(400);
+            }
+            else{
+                //printf("bit %d \n",( 1 & (msg[counter] >> count_bit)));
+                kill(ft_atoi(pid), SIGUSR1);
+                usleep(400);
+            }
+            count_bit--;
         } 
         counter++;
     }
@@ -45,6 +54,6 @@ int main(int ac, char *av[])
     if(ac == 3)
         convert_char(av[1], av[2]);
     else 
-        put_message("client [server PID] [message]", 1);
+        put_message("client [server PID] [message]\n", 1);
     return 0;
 }
--- minitalk/server.c
+++ minitalk/server.c
@@ -11,24 +11,49 @@
 /* ************************************************************************** */
 
 #include"minitalk.h"
+#include"stdio.h"
 
-void handel_signal(int sig)
+void print_char(int sig, siginfo_t *info, void *context)
 {
-    (void)sig;
-    write(1 , &"0\n", 1);
+    static int bit_counter = 0;
+    static unsigned char byte = 0 ; 
+    static int c_pid = 0 ; 
+
+    (void)context ;
+    if(c_pid != info->si_pid)
+    {
+        byte = 0 ;
+        bit_counter = 0 ;
+    }    
+    c_pid = info->si_pid ;
+    byte = byte << 1 ;
+    if(sig == SIGUSR2)
+        byte = byte | 1 ;
+    bit_counter++; 
+    if(bit_counter == 8){
+        write(1 , &byte, 1);
+        byte = 0 ;
+        bit_counter = 0;
+    }
 }
 
 int main(){
     int pid;
-
     struct sigaction sa;
 
-    sa.sa_handler = &handel_signal;
+    sa. sa_sigaction = &print_char;
     pid = getpid();
     put_nbr(pid);
     write(1 , &"\n", 1);
     sigaction(SIGUSR1, &sa, NULL);
     sigaction(SIGUSR2, &sa, NULL);
-    pause();
+    while(1);
     return 0;
 }
+
+/*
+    97 = 64 + 32 +1
+
+    128 64 32 16 8 4 2 1
+    0    1  1  0 0 0 0 1
+ */
